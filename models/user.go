3e98f194fd9bd92bda84ae04fed98090a5c31f48
bjamyl <alhassanjamil0@gmail.com>
2024-01-11 21:18:02 +0000

    add function for verifying jwt tokens and protecting routes

--- models/user.go
+++ models/user.go
@@ -43,7 +43,7 @@ func (u User) Save() error {
 	return err
 }
 
-func (u User) ValidateCredentials() error {
+func (u *User) ValidateCredentials() error {
 	query := "SELECT id, password FROM users WHERE email = ?"
 	row := db.DB.QueryRow(query, u.Email)
 
--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"com.github/bjamyl/go-events-api/models"
+	"com.github/bjamyl/go-events-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -38,16 +39,30 @@ func getEvent(context *gin.Context) {
 
 // Creating an event
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
